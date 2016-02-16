52298766d1dfaef8ea3729254aa50257dd539912
Chris Whiten <chris.whiten@gmail.com>
2016-02-16 16:47:32 -0500

    Use redis to cluster connections

--- server/index.js
+++ server/index.js
@@ -1,23 +1,32 @@
 'use strict';
 
-const server = require('http').createServer();
-const io = require('socket.io')(server);
+var server = require('http').createServer(function(req, res) {
+  console.log('got a request', req.url);
+  if (req.url === '/index.html') {
+    res.writeHead(200);
+    res.end();
+    return;
+  }
+});
+var io = require('socket.io')(server);
+var redis = require('socket.io-redis');
+io.adapter(redis({host: 'localhost', port: 6379}));
 
 // set up the connection
-io.on('connection', (socket) => {
-  socket.on('test event', (data) => {
+io.on('connection', function (socket) {
+  socket.on('test event', function (data) {
     console.log('data:', data);
   });
 
-  socket.on('disconnect', () => {
+  socket.on('disconnect', function () {
     console.log('disconnected');
   });
 });
 
-server.listen(3003);
+server.listen(process.argv[2]);
 
 // repeatedly send out a given message
-const message = process.argv[2];
-setInterval(() => {
-  io.emit('test event', `Hello ${message}`);
-}, 1000);
+var message = process.argv[3];
+setInterval(function () {
+  io.sockets.emit('test event', 'Hello ' + message);
+}, 100);
